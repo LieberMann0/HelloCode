a0db5b1ccc947ef7a005ae03723372bcd91873bf
Администратор <lieber.mann.0@gmail.com>
2021-10-26 16:10:03 +0300

    Add Example 005 Condition If-Else

---
+++ Example005_If_Else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Здравствуйте, уважаемая Маша!");
+}
+else
+{
+    Console.Write("Досвидания, ");
+    Console.Write(username);
+}
